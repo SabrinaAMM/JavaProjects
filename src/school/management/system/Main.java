b6015d4c6e1460f6cc0452cb29b35f811ba395bd
Sabrina Ammerl <michaela.ammerl@gmx.de>
2020-11-22 22:17:31 +0100

    added School Management Exercise

---
+++ src/school/management/system/Main.java
@@ -0,0 +1,41 @@
+package school.management.system;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+	public static void main(String[] args) {
+		Teacher tom = new Teacher(1, "Tom", 3000);
+		Teacher anna = new Teacher(2, "Anna", 3000);
+		Teacher sandra = new Teacher(3, "Sandra", 3600);
+		
+		List<Teacher> teacherList = new ArrayList<>();
+		teacherList.add(tom);
+		teacherList.add(anna);
+		teacherList.add(sandra);
+		
+		Student lisa = new Student(1, "Lisa", 4);
+		Student christian = new Student(2, "Christian", 5);
+		Student sebastian = new Student(2, "Sebastian", 10);
+		
+		List<Student> studentList = new ArrayList<>();
+		studentList.add(lisa);
+		studentList.add(christian);
+		studentList.add(sebastian);
+		
+		
+		School jgg = new School(teacherList, studentList);
+		System.out.println("Earnings: " + jgg.getTotalEarnings() + " €");
+		
+		lisa.payFees(200);
+		christian.payFees(300);
+		
+		System.out.println("JGG has earned €"+ jgg.getTotalEarnings());
+		
+		
+		System.out.println("Paying Salary....");
+		tom.receiveSalary(tom.getSalary());
+		System.out.println("JGG has spent for salary " + jgg.getTotalEarnings());
+	}
+
+}
---
+++ src/school/management/system/School.java
@@ -0,0 +1,56 @@
+package school.management.system;
+
+import java.util.List;
+
+public class School {
+	private List<Teacher> teachers;
+	private List<Student> students;
+	private static int totalEarnings;
+	private static int totalSpendings;
+	
+	
+	
+	public School(List<Teacher> teachers, List<Student> students) {
+		super();
+		this.teachers = teachers;
+		this.students = students;
+		totalEarnings = 0;
+		totalSpendings = 0;
+	}
+
+	public List<Teacher> getTeachers() {
+		return teachers;
+	}
+
+	
+	public void addTeacher(Teacher teacher) {
+		teachers.add(teacher);
+	}
+
+	public List<Student> getStudents() {
+		return students;
+	}
+
+	public void addStudent(Student student) {
+		students.add(student);
+	}
+
+	public int getTotalEarnings() {
+		return totalEarnings;
+	}
+
+	public static void updateTotalEarnings(int earnings) {
+		totalEarnings += earnings;
+	}
+
+	public int getTotalSpendings() {
+		return totalSpendings;
+	}
+
+	public static void updateTotalSpendings(int spendings) {
+		totalSpendings += spendings;
+	}
+	
+	
+
+}
